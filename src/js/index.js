132b9963e7396b5c52def7f80f34c0b2746c5ef5
Branson <bransonlee0426@gmail.com>
2019-01-31 15:19:46 +0800

    navbar done

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-  
+
+    //Listeing media to fix the hover effect.
+    let CurWidth = window.matchMedia("(max-width: 767px)");
+    CurWidth.addListener(resizeWidth);
+    resizeWidth(CurWidth);
+    
+    function resizeWidth(pMatchMedia){
+      if (pMatchMedia.matches) {
+        $('.item').removeClass('hvr-underline-reveal');
+      }else{
+        $('.item').addClass('hvr-underline-reveal');
+      }
+    }
+    //showmenu
+    $('.showmenu').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('menu-show');
+    });
 });
