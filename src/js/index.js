21a281c007ee85d0de78ff7e4512bd8692e8f9dc
Branson <bransonlee0426@gmail.com>
2019-02-02 13:49:31 +0800

    navbar done

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,46 @@
 $(document).ready(function () {
+  //Listeing media to fix the hover effect.
+  let CurWidth = window.matchMedia("(max-width: 767px)");
+  CurWidth.addListener(resizeWidth);
+  resizeWidth(CurWidth);
 
-    //Listeing media to fix the hover effect.
-    let CurWidth = window.matchMedia("(max-width: 767px)");
-    CurWidth.addListener(resizeWidth);
-    resizeWidth(CurWidth);
-    
-    function resizeWidth(pMatchMedia){
-      if (pMatchMedia.matches) {
-        $('.item').removeClass('hvr-underline-reveal');
-      }else{
-        $('.item').addClass('hvr-underline-reveal');
-      }
+  function resizeWidth(pMatchMedia) {
+    if (pMatchMedia.matches) {
+      $('.item').removeClass('hvr-underline-reveal');
+    } else {
+      $('.item').addClass('hvr-underline-reveal');
     }
-    //showmenu
-    $('#showmenu').on('click', function (e) {
-        e.preventDefault();
-        $('body').toggleClass('menu-show');
-    });
-    //navbar icon animation
-    $('#showmenu').click(function(){
-		$(this).toggleClass('open');
-	});
+  }
+  //showmenu
+  $('#showmenu').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('menu-show');
+  });
+  //navbar icon animation
+  $('#showmenu').click(function () {
+    $(this).toggleClass('open');
+  });
+  let mySwiper = new Swiper('.swiper-container', {
+    
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+    },
+    fadeEffect: {
+      crossFade: true,
+    },
+  })
 });
